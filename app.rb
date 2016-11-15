c0020294a15b2272e2bdf9209e53e0aba2393c69
tiniahung <hsiaotih@andrew.cmu.edu>
2016-11-15 18:36:34 -0500

    t2

--- app.rb
+++ app.rb
@@ -86,6 +86,10 @@ client = Twilio::REST::Client.new "AC3157cd21b96c6f0acb6d118749e10991", "61fe77a
 
 # Use this method to check if your ENV file is set up
 
+get '/' do
+Hello
+end
+
 get "/from" do
   #401
   "+14126936852"
@@ -94,14 +98,15 @@ end
 # Test sending an SMS
 # change the to to your number 
 
-get "/send_sms/" do 
+get '/send_sms/' do 
 
   client.account.messages.create(
     :from => "+14126936852",
     :to => "+14128166195",
     :body => "Hey there. This is a test"
   )
-  "Sent message"
+
+  "Sent message".to_s
   
 end
 
@@ -137,4 +142,87 @@ end
 
 error 401 do 
   "Not allowed!!!"
+end
+
+get '/incoming_sms' do
+  
+  session["last_context"] ||= nil
+  
+  sender = params[:From] || ""
+  body = params[:Body] || ""
+  body = body.downcase.strip
+  
+  if body == "hi" or body == "hello" or body == "hey"
+    message = get_about_message
+  elsif body == "play"
+    session["last_context"] = "play"
+    session["guess_it"] = rand(1...5)
+    message = "Guess what number I'm thinking of. It's between 1 and 5"
+  elsif session["last_context"] == "play"
+    
+    # if it's not a number 
+    if not body.to_i.to_s == body
+      message = "Cheater cheater that's not a number. Try again"
+    elsif body.to_i == session["guess_it"]
+      message = "Bingo! It was #{session["guess_it"]}"
+      session["last_context"] = "correct_answer"
+      session["guess_it"] = -1
+    else
+      message = "Wrong! Try again"
+    end
+    
+  elsif body == "who"
+    message = "I was made by Tina."
+  elsif body == "what"
+    message = "I don't do much but I do it well. You can ask me who what when where or why."
+  elsif body == "when"    
+    message = Time.now.strftime( "It's %A %B %e, %Y")
+  elsif body == "where"    
+    message = "I'm in Pittsburgh right now."
+  elsif body == "why"    
+    message = "For educational purposes."
+  else 
+    message = error_response
+    session["last_context"] = "error"
+  end
+  
+  twiml = Twilio::TwiML::Response.new do |r|
+    r.Message message
+  end
+  twiml.text
+end
+
+
+
+private 
+
+
+
+
+
+GREETINGS = ["Hi","Yo", "Hey","Howdy", "Hello", "Ahoy", "‘Ello", "Aloha", "Hola", "Bonjour", "Hallo", "Ciao", "Konnichiwa"]
+
+COMMANDS = "hi, who, what, where, when, why and play."
+
+def get_commands
+  error_prompt = ["I know how to: ", "You can say: ", "Try asking: "].sample
+  
+  return error_prompt + COMMANDS
+end
+
+def get_greeting
+  return GREETINGS.sample
+end
+
+def get_about_message
+  get_greeting + ", I\'m SMSBot 🤖. " + get_commands
+end
+
+def get_help_message
+  "You're stuck, eh? " + get_commands
+end
+
+def error_response
+  error_prompt = ["I didn't catch that.", "Hmmm I don't know that word.", "What did you say to me? "].sample
+  error_prompt + " " + get_commands
 end
